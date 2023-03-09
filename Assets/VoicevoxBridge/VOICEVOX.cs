c5b3f1d041bf3a42ab5dbd43c6f24e60d4749c3a
Mikito Yoshiya <mikito@MikitoMBP.local>
2023-03-09 15:57:38 +0900

    リファクタ前の命名を修正

--- Assets/VoicevoxBridge/VOICEVOX.cs
+++ Assets/VoicevoxBridge/VOICEVOX.cs
@@ -10,33 +10,33 @@ namespace VoicevoxBridge
         [SerializeField] string voicevoxEngineURL = "http://localhost:50021/";
         [SerializeField] bool enableLog = false;
 
-        VoicevoxPlayer controller = null;
+        VoicevoxPlayer player = null;
 
         void Awake()
         {
             if (audioSource == null) audioSource = gameObject.AddComponent<AudioSource>();
-            controller = new VoicevoxPlayer(voicevoxEngineURL, audioSource);
-            controller.EnableLog = enableLog;
+            player = new VoicevoxPlayer(voicevoxEngineURL, audioSource);
+            player.EnableLog = enableLog;
         }
 
         public async Task<Voice> CreateVoice(int speaker, string text, CancellationToken cancellationToken = default)
         {
-            return await controller.CreateVoice(speaker, text, cancellationToken);
+            return await player.CreateVoice(speaker, text, cancellationToken);
         }
 
         public async Task Play(Voice voice, CancellationToken cancellationToken = default, bool autoReleaseVoice = true)
         {
-            await controller.Play(voice, cancellationToken, autoReleaseVoice);
+            await player.Play(voice, cancellationToken, autoReleaseVoice);
         }
 
         public async Task PlayOneShot(int speaker, string text, CancellationToken cancellationToken = default)
         {
-            await controller.PlayOneShot(speaker, text, cancellationToken);
+            await player.PlayOneShot(speaker, text, cancellationToken);
         }
 
         void OnDestroy()
         {
-            if (controller != null) controller.Dispose();
+            if (player != null) player.Dispose();
         }
     }
 }
