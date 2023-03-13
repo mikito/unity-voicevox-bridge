ac94ed1d44da9178a13b2b82c20e885bea2636a7
Mikito Yoshiya <mikito@MikitoMBP.local>
2023-03-14 01:56:45 +0900

    stream.ReadAsyncによるAudioClip変換を実装

--- Assets/VoicevoxBridge/Core/Util/AudioClipUtil.cs
+++ Assets/VoicevoxBridge/Core/Util/AudioClipUtil.cs
@@ -1,6 +1,7 @@
-using System.Collections;
 using UnityEngine;
 using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace VoicevoxBridge
 {
@@ -8,7 +9,7 @@ namespace VoicevoxBridge
     {
         public static AudioClip CreateFromBytes(byte[] data)
         {
-            int channels = data[22];
+            int channels = BitConverter.ToInt16(data, 22);
             int frequency = BitConverter.ToInt32(data, 24);
             int length = data.Length - 44;
             float[] samples = new float[length / 2];
@@ -21,6 +22,64 @@ namespace VoicevoxBridge
 
             AudioClip audioClip = AudioClip.Create("AudioClip", samples.Length, channels, frequency, false);
             audioClip.SetData(samples, 0);
+
+            return audioClip;
+        }
+
+        const int BufferSize = 4096;
+
+        public static async Task<AudioClip> CreateFromStreamAsync(Stream stream)
+        {
+            byte[] ftmChank = new byte[44];
+
+            await stream.ReadAsync(ftmChank, 0, 44);
+            int channels = BitConverter.ToInt16(ftmChank, 22);
+            int bitPerSample = BitConverter.ToInt16(ftmChank, 34);
+
+            if (channels != 1) throw new NotSupportedException("AudioClipUtil supports only single channel.");
+            if (bitPerSample != 16) throw new NotSupportedException("AudioClipUtil supports only 16-bit quantization.");
+
+            int bytePerSample = bitPerSample / 8;
+            int frequency = BitConverter.ToInt32(ftmChank, 24);
+            int length = BitConverter.ToInt32(ftmChank, 40);
+
+            AudioClip audioClip = null;
+            try
+            {
+                audioClip = AudioClip.Create("AudioClip", length / 2, channels, frequency, false);
+
+                byte[] readBuffer = new byte[BufferSize];
+                float[] samplesBuffer = new float[BufferSize / bytePerSample];
+                int offset = 0;
+                int read;
+
+                while ((read = await stream.ReadAsync(readBuffer, 0, readBuffer.Length)) > 0)
+                {
+                    // Supports only 16-bit quantization, and single channel.
+                    for (int i = 0; i < read / bytePerSample; i++)
+                    {
+                        short value = BitConverter.ToInt16(readBuffer, i * bytePerSample);
+                        samplesBuffer[i] = value / 32768f;
+                    }
+
+                    if (read == BufferSize)
+                    {
+                        audioClip.SetData(samplesBuffer, offset);
+                    }
+                    else
+                    {
+                        var lastSamples = new float[read / bytePerSample];
+                        Array.Copy(samplesBuffer, lastSamples, read / bytePerSample);
+                        audioClip.SetData(lastSamples, offset);
+                    }
+                    offset += read / 2;
+                }
+            }
+            catch(Exception e)
+            {
+                if (audioClip != null) UnityEngine.Object.Destroy(audioClip);
+                throw new IOException("AudioClipUtil: WAV data decode failed.", e);
+            }
 
             return audioClip;
         }
